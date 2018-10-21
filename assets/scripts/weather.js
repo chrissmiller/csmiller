f50c24845d674bba4901d92f79f4d782b776ba93
chrissmiller <chris.20@dartmouth.edu>
2018-10-20 22:02:29 -0400

    Added functionality to get weather in your location

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,3 +1,12 @@
+/*
+	This program uses the OpenWeatherMap API to access the temperature in Lebanon, and
+	tells the user if it is a good skiing/snowboarding temperature (defined as between -10 and 32 degrees Fahrenheit).
+
+	It also allows the user to check the temperature at their own location and provides the same functionality.
+	Sorry for any sloppy styling here - I haven't used JS before but it was fun to learn this bit!
+
+	Due to free API limitations - may be slow if user makes a lot of calls in rapid succession
+*/
 "use strict";
 var request = new XMLHttpRequest();
 
@@ -7,27 +16,83 @@ function callback(){
 	}
 }
 
+//Sets HTML for default onload behavior (parses data and assigns to the initial text node)
 function setHTML(response){
 	let weatherData = JSON.parse(response);
 	let msg = `Temperature in Lebanon, NH: ${weatherData.main.temp} °F. `;
-	if (weatherData.main.temp > 32){
-		msg += 'Too warm for snowboarding!'
-	} else if (weatherData.main.temp < -10){
-		msg += 'Too cold for snowboarding!'
-	} else {
-		msg += 'Looks like great weather for snowboarding!'
-	}
+	msg += howCold(weatherData.main.temp);
 	let weatherNode = document.createElement('h4');
 	let weatherText = document.createTextNode(msg);
+	weatherNode.id = "weatherNodeLoc";
 	weatherNode.appendChild(weatherText);
 	document.getElementById('weatherSection').appendChild(weatherNode);
 }
 
-const APIKEY = "6cc0896b2cbd9c679194b07bb6fabfbb";
-let cityID = 5088597;
-let callURL = `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${APIKEY}&units=imperial`
-request.open('GET', callURL, true);
-request.onload = callback;
+//Returns the API call URL for Lebanon
+function getDefaultCallURL(){
+	const APIKEY = "6cc0896b2cbd9c679194b07bb6fabfbb";
+	let cityID = 5088597;
+	return `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${APIKEY}&units=imperial`
+}
 
-request.send();
-//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}
+//Gets a user location and calls the userCallURL function
+//Sets button to "working" since the call takes a moment
+function getUserLocation(){
+	let weatherButton = document.getElementById('weatherButton');
+	weatherButton.innerHTML = "Working...";
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(userCallURL);
+	}
+}
+
+//Gets the API call URL for the user's location
+function userCallURL(position){
+	const APIKEY = "6cc0896b2cbd9c679194b07bb6fabfbb";
+	let callURL = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${APIKEY}&units=imperial`;
+
+	request.open('GET', callURL, true);
+	request.onload = userCallback;
+	request.send();
+}
+
+//Parses user location weather data and amends the weather reporting text to provide it.
+function userCallback(){
+	let weatherNode = document.getElementById("weatherNodeLoc");
+	let msg;
+
+	if(request.status >= 200 && request.status < 400){
+		let userWeatherData = JSON.parse(this.response);
+		msg = `Temperature in your location: ${userWeatherData.main.temp} °F. `
+		msg += howCold(userWeatherData.main.temp);
+	} else {
+		msg = "Unable to access the weather at your location."
+	}
+	let weatherText = document.createTextNode(msg);
+	weatherNode.removeChild(weatherNode.lastChild);
+	weatherNode.appendChild(weatherText);
+
+	let weatherButton = document.getElementById('weatherButton');
+	weatherButton.innerHTML = "Get your weather!";
+}
+
+//Reports if it is too cold or too warm for snowboarding
+function howCold(temp){
+	let upperBound = 32;
+	let lowerBound = -10;
+
+	if (temp > upperBound){
+		return 'Too warm for snowboarding!'
+	} else if (temp < lowerBound){
+		return 'Too cold for snowboarding!'
+	} else {
+		return 'Looks like great weather for snowboarding!'
+	}
+}
+
+//Calls the default weather checking behavior
+function sysGetWeather(){
+	let callURL = getDefaultCallURL();
+	request.open('GET', callURL, true);
+	request.onload = callback;
+	request.send();
+}
